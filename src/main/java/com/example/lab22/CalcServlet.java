f7886a651f2263687e5af31f5187b9850c4dd5fc
Jakub <kubasuchanowski@gmail.com>
2022-10-11 14:45:01 +0200

    +ciasteczka

--- src/main/java/com/example/lab22/CalcServlet.java
+++ src/main/java/com/example/lab22/CalcServlet.java
@@ -8,23 +8,69 @@ import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @WebServlet(name = "CalcServlet", value = "/CalcServlet")
 public class CalcServlet extends HttpServlet {
 
-    public String message;
-    public void init(){
-        message= "Nie podano liczby";
-    }
-
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws  IOException {
 
+        /*
+        String ileWizyt = "1";
+        Cookie [] cookies = request.getCookies();
+        if(cookies !=null ){
+            for(int i=0;i<cookies.length;i++){
+                Cookie cookie = cookies[i];
+                if(ileWizyt.equals(cookie.getName())) ileWizyt = cookie.getValue();
+            }
+        }
+        int ile =1;
+        try{
+            ile = Integer.parseInt(ileWizyt);
+        }
+        catch(NumberFormatException nfe){}
+*/
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException {
+
+        List<String> history = new ArrayList<>();
+        HttpSession session = request.getSession(true);
+
+        String ileWizyt = "1";
+        Cookie [] cookies = request.getCookies();
+        if(cookies !=null ){
+            for(int i=0;i<cookies.length;i++){
+                Cookie cookie = cookies[i];
+                if("ileWizyt".equals(cookie.getName())) ileWizyt = cookie.getValue();
+            }
+        }
+        String informacja="";
+        int ile =1;
+        try{
+            ile = Integer.parseInt(ileWizyt);
+            if(ile == 1) {informacja = "Witaj po raz pierwszy!";}
+            else {informacja= "Witaj po raz kolejny!";}
+
+        }
+        catch(NumberFormatException nfe){}
+
+        Cookie c = new Cookie("ileWizyt",String.valueOf(ile+1));
+        response.addCookie(c);
+
+
+
+        response.setContentType("text/html; charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html><head><title></title></head><body><h2>" + informacja +
+                        "</h2><h3>Strona została wyświetlona przez tę przeglądarkę " + ile + " razy.</h3></body></html>");
+
+
         String operation;
         Double parametr1=null;
         Double parametr2=null;
